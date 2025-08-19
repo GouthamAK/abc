2f92b20affe775df48dc265a09333c4de95b3927
GouthamAK <220701077@rajalakshmi.edu.in>
2025-08-19 09:46:21 +0530

    Commit

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace conflict
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
